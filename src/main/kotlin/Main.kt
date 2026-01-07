80c83e2fcacb45dbe913aadc8c3e228dc79f50a7
NiriLorentzen <niri.lorentzen@gmail.com>
2026-01-07 10:03:53 +0100

    fikset imports

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,8 +1,12 @@
 package org.example
 
-import com.sun.net.httpserver.Request
-import okhttp3.*
+
 import java.io.IOException
+import okhttp3.OkHttpClient
+import okhttp3.Request
+import okhttp3.Callback
+import okhttp3.Call
+import okhttp3.Response
 
 
 
@@ -10,7 +14,7 @@ fun main() {
     val client = OkHttpClient()
 
     val request = Request.Builder()
-        .url("https://jsonplaceholder.typicode.com/posts/1")
+        .url("https://asrv.avinor.no/XmlFeed/v1.0?TimeFrom=1&TimeTo=7&airport=OSL&direction=D&lastUpdate=2024-08-08T09:30:00Z")
         .build()
 
     client.newCall(request).enqueue(object : Callback {

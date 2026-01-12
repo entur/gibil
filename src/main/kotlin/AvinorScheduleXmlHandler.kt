70e0e6d107762e3095022c5e26ec45fbc7e777c2
Eivind-HB <eivind.h.breivik@gmail.com>
2026-01-12 14:13:27 +0100

    Added marshall function to AvinorScheduleXmlHandler

--- src/main/kotlin/AvinorScheduleXmlHandler.kt
+++ src/main/kotlin/AvinorScheduleXmlHandler.kt
@@ -1,21 +1,40 @@
 package org.example
 
 import jakarta.xml.bind.JAXBContext
+import jakarta.xml.bind.Marshaller
 import org.example.netex.Airport
+import java.io.IOException
 import java.io.StringReader
+import java.io.StringWriter
 
 class AvinorScheduleXmlHandler {
 
+    companion object {
+        private val context: JAXBContext = JAXBContext.newInstance(Airport::class.java)
+    }
+
     fun unmarshall(xmlData: String): Airport {
-        val context = JAXBContext.newInstance(Airport::class.java)
-        val unmarshaller = context.createUnmarshaller()
-
         try {
-            val airportData: Airport = unmarshaller.unmarshal(StringReader(xmlData)) as Airport
+            val unmarshaller = context.createUnmarshaller()
+            return unmarshaller.unmarshal(StringReader(xmlData)) as Airport
 
-            return airportData
         } catch (e: Exception) {
             throw RuntimeException("Error parsing Airport", e)
+        }
+    }
+
+    fun marshall(airport: Airport): String {
+        try {
+            val marshaller = context.createMarshaller().apply {
+                setProperty(Marshaller.JAXB_FORMATTED_OUTPUT, true)
+                setProperty(Marshaller.JAXB_ENCODING, "UTF-8")
+            }
+            return StringWriter().use { writer ->
+                marshaller.marshal(airport, writer)
+                writer.toString()
+            }
+        } catch (e: IOException) {
+            throw RuntimeException("Error marshalling airport", e)
         }
     }
 

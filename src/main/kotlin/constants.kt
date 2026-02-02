cb9ae0f0d939b9e94e78a151a7693a22d93e0b49
Eivind-HB <eivind.h.breivik@gmail.com>
2026-02-02 12:15:31 +0100

    Removed redundant constants

--- src/main/kotlin/constants.kt
+++ src/main/kotlin/constants.kt
@@ -9,11 +9,5 @@ package org.example
 const val BATCH_SIZE = 5
 const val REQUEST_DELAY_MS = 50
 
-//AvinorApiHandler
-const val TIMEFROMPARAM_MIN_NUM = 1
-const val TIMEFROMPARAM_MAX_NUM = 36
-const val TIMETOPARAM_MIN_NUM = 7
-const val TIMETOPARAM_MAX_NUM = 336
-
 object Constants {
 }

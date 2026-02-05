bbd0aada2d36627277a1be07dde8c338056a10b6
Eivind-HB <eivind.h.breivik@gmail.com>
2026-02-05 15:54:41 +0100

    Separated concerns and created AirportQuayService

---
+++ src/main/kotlin/service/AirportQuayService.kt
@@ -0,0 +1,27 @@
+package org.gibil.service
+
+import jakarta.annotation.PostConstruct
+import org.gibil.StopPlaceMapper
+import org.gibil.routes.api.StopPlaceApiHandler
+import org.springframework.stereotype.Service
+
+@Service
+class AirportQuayService(private val handler: StopPlaceApiHandler, private val mapper: StopPlaceMapper) {
+
+    private var iataToQuayMap: Map<String, List<String>> = emptyMap()
+
+    @PostConstruct
+    fun init() {
+        refreshQuayMapping()
+    }
+
+    fun refreshQuayMapping() {
+        val xml = handler.fetchAirportStopPlaces() ?: return
+        val stopPlaces = mapper.unmarhsallStopPlaceXml(xml)
+        iataToQuayMap = mapper.makeIataToQuayMap(stopPlaces)
+    }
+
+    fun getQuayIds(iataCode: String): List<String> {
+        return iataToQuayMap[iataCode] ?: emptyList()
+    }
+}

9aea26a2a24e151ec3729500b79871b1e97c8e42
Eivind-HB <eivind.h.breivik@gmail.com>
2026-02-06 14:18:15 +0100

    Added K-docs to StopPlaceApiHandler.kt

--- src/main/kotlin/routes/api/StopPlaceApiHandler.kt
+++ src/main/kotlin/routes/api/StopPlaceApiHandler.kt
@@ -10,6 +10,12 @@ class StopPlaceApiHandler(private val apiService: ApiService) {
         const val BASE_URL_STOP_PLACES = "https://api.entur.io/stop-places/v1/read/stop-places"
     }
 
+    /**
+     * Builds url used for StopPlaces [API] from Entur, defaults to 100 airports,
+     * [AIR] transportModes, [AIRPORT] stop place type. Other functions must be expanded to use other modes
+     * @param stopPlaceCount, [transportModes], [stopPlaceTypes]
+     * @return String, built url
+     */
     fun stopPlaceApiUrlBuilder(stopPlaceCount: Int = 100, transportModes: String = "AIR", stopPlaceTypes: String = "AIRPORT"): String = buildString {
         append(StopPlaceApiHandlerConfig.BASE_URL_STOP_PLACES)
         if(stopPlaceCount > 0){
@@ -19,6 +25,11 @@ class StopPlaceApiHandler(private val apiService: ApiService) {
         append("&stopPlaceTypes=$stopPlaceTypes")
     }
 
+    /**
+     * Makes use of [ApiService] to make the API call to Enturs StopPlaces Api
+     * Asks for XML data
+     * @return String?, XML response from API
+     */
     fun fetchAirportStopPlaces(): String? {
         val url = stopPlaceApiUrlBuilder()
         return apiService.apiCall(url, "application/xml")

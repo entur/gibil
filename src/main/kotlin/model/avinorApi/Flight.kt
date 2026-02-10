cd04975a39fe1155036307ac74ab73e67ee2f493
William Jondahl <142999366+Williamjondahl@users.noreply.github.com>
2026-02-10 10:03:36 +0100

    Merged flights (#50)
    * Expanded Flight model to support merged flights, containing both arrival and departure times in one object. Merges using a flight's uniqueID
    
    * Created FlightAggregationService which aggregates all domestic flights and merges the departure/arrival records by the flights uniqueID.
    
    * Modified SiriETMapper.kt to support merged flights. New function mapMergedFlightsToSiri() entrypoint for merged flight objects. Modified various functions to handle the departure and arrival time fields for scheduledTime
    
    * added commented out /all endpoints which returns raw api response or siri data for all airports avinor supports

--- src/main/kotlin/model/avinorApi/Flight.kt
+++ src/main/kotlin/model/avinorApi/Flight.kt
@@ -4,6 +4,7 @@ import jakarta.xml.bind.annotation.XmlAccessType
 import jakarta.xml.bind.annotation.XmlAccessorType
 import jakarta.xml.bind.annotation.XmlAttribute
 import jakarta.xml.bind.annotation.XmlElement
+import jakarta.xml.bind.annotation.XmlTransient
 
 @XmlAccessorType(XmlAccessType.FIELD)
 class Flight {
@@ -56,6 +57,28 @@ class Flight {
     @XmlElement(name = "codeshareOperationalSuffixs")
     var OperationalSuffixs: String? = null
 
+    // These fields are populated when merging flight data from multiple airport queries.
+    @XmlTransient
+    var departureAirport: String? = null
+
+    @XmlTransient
+    var arrivalAirport: String? = null
+
+    @XmlTransient
+    var scheduledDepartureTime: String? = null
+
+    @XmlTransient
+    var scheduledArrivalTime: String? = null
+
+    @XmlTransient
+    var departureStatus: FlightStatus? = null
+
+    @XmlTransient
+    var arrivalStatus: FlightStatus? = null
+
+    @XmlTransient
+    var isMerged: Boolean = false
+
     //JAXB needs an empty constructor to populate the fields with the xml data
     constructor()
 
@@ -74,4 +97,60 @@ class Flight {
 
     fun isCancelled(): Boolean = status?.code == "C"
     fun isDelayed(): Boolean = delayed == "Y"
+
+    /**
+     * Populates the merged fields based on whether this flight is a departure or arrival.
+     * Call this after parsing from XML before merging with other flights.
+     * @param queryAirportCode The airport code used in the API query
+     */
+    fun populateMergedFields(queryAirportCode: String) {
+        if (isDeparture()) {
+            departureAirport = queryAirportCode
+            arrivalAirport = airport
+            scheduledDepartureTime = scheduleTime
+            departureStatus = status
+        } else {
+            arrivalAirport = queryAirportCode
+            departureAirport = airport
+            scheduledArrivalTime = scheduleTime
+            arrivalStatus = status
+        }
+    }
+
+    /**
+     * Merges data from another Flight with the same uniqueID.
+     * Combines departure data from one airport with arrival data from another.
+     * @param other The other Flight to merge with (must have same uniqueID)
+     * @return A new Flight with combined data from both
+     */
+    fun mergeWith(other: Flight): Flight {
+        if (this.uniqueID != other.uniqueID) {
+            throw IllegalArgumentException("Cannot merge flights with different uniqueIDs: ${this.uniqueID} vs ${other.uniqueID}")
+        }
+
+        return Flight(this.uniqueID).apply {
+            // Basic fields - prefer non-null values
+            airline = this@Flight.airline ?: other.airline
+            flightId = this@Flight.flightId ?: other.flightId
+            domInt = this@Flight.domInt ?: other.domInt
+            viaAirport = this@Flight.viaAirport ?: other.viaAirport
+            delayed = this@Flight.delayed ?: other.delayed
+            AirlineDesignators = this@Flight.AirlineDesignators ?: other.AirlineDesignators
+            AirlineNames = this@Flight.AirlineNames ?: other.AirlineNames
+            FlightNumbers = this@Flight.FlightNumbers ?: other.FlightNumbers
+            OperationalSuffixs = this@Flight.OperationalSuffixs ?: other.OperationalSuffixs
+
+            // Merge departure data
+            departureAirport = this@Flight.departureAirport ?: other.departureAirport
+            scheduledDepartureTime = this@Flight.scheduledDepartureTime ?: other.scheduledDepartureTime
+            departureStatus = this@Flight.departureStatus ?: other.departureStatus
+
+            // Merge arrival data
+            arrivalAirport = this@Flight.arrivalAirport ?: other.arrivalAirport
+            scheduledArrivalTime = this@Flight.scheduledArrivalTime ?: other.scheduledArrivalTime
+            arrivalStatus = this@Flight.arrivalStatus ?: other.arrivalStatus
+
+            isMerged = true
+        }
+    }
 }

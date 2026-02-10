8449a07bb7fdee993127e72de8ce9ae9546dd7b6
Eivind-HB <eivind.h.breivik@gmail.com>
2026-02-10 10:46:35 +0100

    Removed unused printing function

--- src/main/kotlin/service/AirportService.kt
+++ src/main/kotlin/service/AirportService.kt
@@ -25,7 +25,7 @@ import kotlin.system.measureTimeMillis
 class AirportService(
     private val avinorApi: AvinorApiHandler,
     private val avxh: AvinorScheduleXmlHandler,
-    private val apiService: ApiService,
+    private val apiService: ApiService
 ) {
     private val ioDispatcher: CoroutineDispatcher = Dispatchers.IO
 
@@ -69,64 +69,6 @@ class AirportService(
                     )
                 )
             }
-        }
-
-        val results = deferredResults.awaitAll()
-
-        results.forEach { (code, xmlData) ->
-            if (xmlData != null && "Error" !in xmlData) {
-                try {
-                    val airportObject = avxh.unmarshallXmlToAirport(xmlData)
-                    printFlightDetails(airportObject)
-                } catch (e: Exception) {
-                    println("Could not parse data for $code: ${e.message}")
-                }
-            } else {
-                println("Failed to fetch data for $code")
-            }
-        }
-    }
-
-    /**
-     * Prints flight details for a given airport.
-     * @param airportData Airport object containing flight information.
-     */
-    private fun printFlightDetails(airportData: Airport) {
-        try {
-            println("\n--------------------------------------------------")
-            println("AIRPORT: ${airportData.name}")
-            println("%-10s %-10s %-15s %-10s %-20s".format(
-                "FLIGHT",
-                "DIR",
-                "DEST",
-                "TIME",
-                "STATUS"
-            ))
-            println("\n--------------------------------------------------")
-
-            airportData.flightsContainer?.flight?.forEach { flight ->
-                val direction = if (flight.arrDep == "A") "From(A)" else "To  (D)"
-                val scheduledTime = flight.scheduleTime?.substring(11, 16) ?: "??"
-                val newTime = flight.status?.time?.substring(11, 16)
-
-                val statusText = when(flight.status?.code) {
-                    "A" -> "Arrived ($newTime)"
-                    "D" -> "Departed ($newTime)"
-                    "E" -> "New Time: $newTime"
-                    "C" -> "Cancelled"
-                    else -> ""
-                }
-
-                println("%-10s %-10s %-15s %-10s %-20s".format(
-                    flight.flightId,
-                    direction,
-                    flight.airport,
-                    scheduledTime,
-                    statusText
-                ))
-            }
-        } catch (e: Exception) {
-            println("Error printing flight details: ${e.message}")
         }
     }
 }

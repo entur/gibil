13fee118445a6f5afb37a41fb8b3e002d7d8858d
Eivind Hovden Breivik <165715590+Eivind-HB@users.noreply.github.com>
2026-02-12 15:27:35 +0100

    StopPlaceApi tests (#59)
    * Changed stopPlaceApiUrlBuilder into using uriComponentsBuilder, moved constants into companion objects
    
    * Removed redundant import
    
    * Made valid tests for stopPlaceApiUrlBuilder
    
    * Made negative tests for stopPlaceApiUrlBuilder
    
    * Made tests for fetchAirportStopPlaces
    
    * Made service folder in tests. Moved AirportServiceTest.kt
    
    * Added unit tests for refreshQuayMapping
    
    * Added unit tests for getQuayId
    
    * made unit tests for unmmarshallStopPlaceXml
    
    * Removed redundant imports, fixed class naming. added todo
    
    * removed unused files

---
+++ src/test/kotlin/service/AirportQuayServiceTest.kt
@@ -0,0 +1,132 @@
+package service
+
+import io.mockk.every
+import io.mockk.mockk
+import io.mockk.verify
+import org.gibil.StopPlaceMapper
+import org.gibil.model.stopPlacesApi.StopPlaces
+import org.gibil.routes.api.StopPlaceApiHandler
+import org.gibil.service.AirportQuayService
+import org.junit.jupiter.api.BeforeEach
+import org.junit.jupiter.api.Nested
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.assertNull
+import kotlin.test.assertEquals
+
+class AirportQuayServiceTest {
+
+    private lateinit var handler: StopPlaceApiHandler
+    private lateinit var mapper: StopPlaceMapper
+    private lateinit var airportQuayService: AirportQuayService
+
+    @BeforeEach
+    fun init() {
+        handler = mockk()
+        mapper = mockk()
+
+        airportQuayService = AirportQuayService(handler, mapper)
+    }
+
+    @Nested
+    inner class RefreshQuayMapping {
+
+       @Test
+        fun `refreshQuayMapping populates map when API returns valid XML`() {
+
+            val validXml = "<xml>stopPlaces</xml>"
+            val stopPlaces = StopPlaces()
+            val expectedMap = mapOf(
+                "OSL" to listOf("NSR:Quay:1173"),
+                "BGO" to listOf("NSR:Quay:1213")
+            )
+            every { handler.fetchAirportStopPlaces() } returns validXml
+            every { mapper.unmarhsallStopPlaceXml(validXml)} returns stopPlaces
+            every { mapper.makeIataToQuayMap(stopPlaces) } returns expectedMap
+
+            airportQuayService.refreshQuayMapping()
+
+            assertEquals("NSR:Quay:1173", airportQuayService.getQuayId("OSL"))
+            assertEquals("NSR:Quay:1213", airportQuayService.getQuayId("BGO"))
+        }
+
+        @Test
+        fun `refreshQuayMapping does not populates map when API returns null`() {
+            every { handler.fetchAirportStopPlaces() } returns null
+
+            airportQuayService.refreshQuayMapping()
+            verify(exactly = 0) { mapper.unmarhsallStopPlaceXml(any()) }
+            verify(exactly = 0) { mapper.makeIataToQuayMap(any()) }
+        }
+
+        @Test
+        fun `refreshQuayMapping should replace old quay on refresh`() {
+            val initialValidXml = "<xml>stopPlaces</xml>"
+            val initialStopPlaces = StopPlaces()
+            val initialExpectedMap = mapOf(
+                "OSL" to listOf("NSR:Quay:1173"),
+                "BGO" to listOf("NSR:Quay:1213")
+            )
+            every { handler.fetchAirportStopPlaces() } returns initialValidXml
+            every { mapper.unmarhsallStopPlaceXml(initialValidXml)} returns initialStopPlaces
+            every { mapper.makeIataToQuayMap(initialStopPlaces) } returns initialExpectedMap
+
+            airportQuayService.refreshQuayMapping()
+
+            assertEquals("NSR:Quay:1173", airportQuayService.getQuayId("OSL"))
+            assertEquals("NSR:Quay:1213", airportQuayService.getQuayId("BGO"))
+
+            val newValidXml = "<xml>stopPlaces</xml>"
+            val newStopPlaces = StopPlaces()
+            val newExpectedMap = mapOf(
+                "OSL" to listOf("NSR:Quay:9373"),
+                "BGO" to listOf("NSR:Quay:9713")
+            )
+            every { handler.fetchAirportStopPlaces() } returns newValidXml
+            every { mapper.unmarhsallStopPlaceXml(newValidXml)} returns newStopPlaces
+            every { mapper.makeIataToQuayMap(newStopPlaces) } returns newExpectedMap
+
+            airportQuayService.refreshQuayMapping()
+
+            assertEquals("NSR:Quay:9373", airportQuayService.getQuayId("OSL"))
+            assertEquals("NSR:Quay:9713", airportQuayService.getQuayId("BGO"))
+        }
+    }
+
+    @Nested
+    inner class GetQuayId {
+
+        @BeforeEach
+        fun setUp() {
+            val validXml = "<xml>stopPlaces</xml>"
+            val stopPlaces = StopPlaces()
+            val expectedMap = mapOf(
+                "OSL" to listOf("NSR:Quay:1173"),
+                "BGO" to listOf("NSR:Quay:1213")
+            )
+            every { handler.fetchAirportStopPlaces() } returns validXml
+            every { mapper.unmarhsallStopPlaceXml(validXml)} returns stopPlaces
+            every { mapper.makeIataToQuayMap(stopPlaces) } returns expectedMap
+
+            airportQuayService.refreshQuayMapping()
+        }
+
+        @Test
+        fun `getQuayId returns expected value`() {
+            val resultOSL = airportQuayService.getQuayId("OSL")
+            val resultBGO = airportQuayService.getQuayId("BGO")
+
+            assertEquals("NSR:Quay:1173", resultOSL)
+            assertEquals("NSR:Quay:1213", resultBGO)
+        }
+
+        @Test
+        fun `getQuayId returns null when airport does not have a quay`() {
+            val result = airportQuayService.getQuayId("KRS")
+
+            assertNull(result)
+        }
+
+
+    }
+
+}

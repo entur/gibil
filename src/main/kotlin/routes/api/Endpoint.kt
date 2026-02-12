86d39a13dacdb257336879a4b8a5eb10513e4f90
NicolayLT <nicolayl@uia.no>
2026-02-12 13:18:00 +0100

    Removed unused import in endpoint file.

--- src/main/kotlin/routes/api/Endpoint.kt
+++ src/main/kotlin/routes/api/Endpoint.kt
@@ -6,7 +6,6 @@ import org.springframework.http.MediaType
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestParam
 import org.springframework.web.bind.annotation.RestController
-import service.FilterExtimeAndFindServiceJourney
 import service.FlightAggregationService
 import service.SiriEtService
 import siri.SiriETMapper

dc2a35b460cdf92c47cbcc95e570378cab301b7f
NicolayLT <nicolayl@uia.no>
2026-02-17 13:34:31 +0100

    Added model to spirngboot scanner

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -4,7 +4,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 import org.springframework.scheduling.annotation.EnableScheduling
 
-@SpringBootApplication(scanBasePackages = ["org.gibil", "routes.api", "config", "siri", "handler", "service", "subscription"])
+@SpringBootApplication(scanBasePackages = ["org.gibil", "routes.api", "config", "siri", "handler", "service", "subscription", "model"])
 @EnableScheduling
 class Application
 

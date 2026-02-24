01f3f16a16be4a8e516d49cf88368eb5b9d940ca
NiriLorentzen <144032550+NiriLorentzen@users.noreply.github.com>
2026-02-24 14:14:44 +0100

    Api service unittests (#94)
    * Added some unittests for apiservice
    
    * Added some unittests for apiservice
    
    * reverted change
    
    Updated airport code in test and removed unused test case.

---
+++ src/test/kotlin/service/ApiServiceTest.kt
@@ -0,0 +1,193 @@
+package service
+
+import io.mockk.every
+import io.mockk.mockk
+import io.mockk.slot
+import okhttp3.OkHttpClient
+import okhttp3.Call
+import okhttp3.Response
+import okhttp3.Request
+import okhttp3.ResponseBody
+import org.gibil.service.ApiService
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.Assertions
+import java.io.File
+import java.io.IOException
+
+class ApiServiceTest {
+    private val mockClient = mockk<OkHttpClient>()
+    private val mockCall = mockk<Call>()
+    private val mockResponse = mockk<Response>()
+    private val mockBody = mockk<ResponseBody>()
+
+    private val apiService = ApiService(mockClient)
+
+    val fileContent = "<xml>data</xml>"
+    val inputStream = fileContent.byteInputStream()
+
+    @Test
+    fun `apiCall returns body string on success`() {
+        every { mockClient.newCall(any()) } returns mockCall
+        every { mockCall.execute() } returns mockResponse
+        every { mockResponse.isSuccessful } returns true
+        every { mockResponse.body } returns mockBody
+        every { mockBody.string() } returns "<xml>data</xml>"
+        every { mockResponse.close() } returns Unit
+
+        val result = apiService.apiCall("https://example.com")
+
+        Assertions.assertEquals("<xml>data</xml>", result)
+    }
+
+    @Test
+    fun `apiCall returns null when body is null`() {
+        every { mockClient.newCall(any()) } returns mockCall
+        every { mockCall.execute() } returns mockResponse
+        every { mockResponse.isSuccessful } returns true
+        every { mockResponse.body } returns null
+        every { mockResponse.close() } returns Unit
+
+        val result = apiService.apiCall("https://example.com")
+
+        Assertions.assertNull(result)
+    }
+
+    @Test
+    fun `apiCall throws IOException on failure`() {
+        every { mockClient.newCall(any()) } returns mockCall
+        every { mockCall.execute() } returns mockResponse
+        every { mockResponse.isSuccessful } returns false
+        every { mockResponse.code } returns 404
+        every { mockResponse.close() } returns Unit
+
+        Assertions.assertThrows(IOException::class.java) {
+            apiService.apiCall("https://example.com")
+        }
+    }
+
+    @Test
+    fun `apiCall contains acceptHeader when provided`() {
+        //make sure to capture request sent
+        val requestSlot = slot<Request>()
+        every { mockClient.newCall(capture(requestSlot)) } returns mockCall
+
+        every { mockCall.execute() } returns mockResponse
+        every { mockResponse.isSuccessful } returns true
+        every { mockResponse.body } returns mockBody
+        every { mockBody.string() } returns "<xml>data</xml>"
+        every { mockResponse.close() } returns Unit
+
+        apiService.apiCall("https://example.com", acceptHeader = "application/xml")
+
+        Assertions.assertEquals("application/xml", requestSlot.captured.header("Accept"))
+    }
+
+    @Test
+    fun `apiCall shouldn't contain acceptHeader when not provided`() {
+        //make sure to capture request sent
+        val requestSlot = slot<Request>()
+        every { mockClient.newCall(capture(requestSlot)) } returns mockCall
+
+        every { mockCall.execute() } returns mockResponse
+        every { mockResponse.isSuccessful } returns true
+        every { mockResponse.body } returns mockBody
+        every { mockBody.string() } returns "<xml>data</xml>"
+        every { mockResponse.close() } returns Unit
+
+        apiService.apiCall("https://example.com")
+
+        Assertions.assertEquals(null, requestSlot.captured.header("Accept"))
+    }
+
+    @Test
+    fun `apiCallToFile writes to file and file contains expected content`() {
+        val targetFile = File.createTempFile("test", ".xml")
+
+        every { mockClient.newCall(any()) } returns mockCall
+        every { mockCall.execute() } returns mockResponse
+        every { mockResponse.isSuccessful } returns true
+        every { mockResponse.body } returns mockBody
+        every { mockBody.byteStream() } returns inputStream
+        every { mockResponse.close() } returns Unit
+
+        apiService.apiCallToFile("https://example.com", targetFile)
+
+        Assertions.assertEquals(fileContent, targetFile.readText())
+        targetFile.deleteOnExit()
+    }
+
+    @Test
+    fun `apiCallToFile writes empty string to file when response is null`() {
+        val targetFile = File.createTempFile("test", ".xml")
+
+        every { mockClient.newCall(any()) } returns mockCall
+        every { mockCall.execute() } returns mockResponse
+        every { mockResponse.isSuccessful } returns true
+        every { mockResponse.body } returns null
+        every { mockResponse.close() } returns Unit
+
+        apiService.apiCallToFile("https://example.com", targetFile)
+
+        Assertions.assertEquals("", targetFile.readText())
+        targetFile.deleteOnExit()
+    }
+
+    @Test
+    fun `apiCallToFile throws IOException when request fails`() {
+        val targetFile = File.createTempFile("test", ".xml")
+
+        every { mockClient.newCall(any()) } returns mockCall
+        every { mockCall.execute() } returns mockResponse
+        every { mockResponse.isSuccessful } returns false
+        every { mockResponse.code } returns 404
+        every { mockResponse.close() } returns Unit
+
+        Assertions.assertThrows(IOException::class.java) {
+            apiService.apiCallToFile("https://example.com", targetFile)
+        }
+
+        targetFile.deleteOnExit()
+    }
+
+    @Test
+    fun `apiCallToFile contains acceptHeader when provided`() {
+        //make sure to capture request sent
+        val requestSlot = slot<Request>()
+        every { mockClient.newCall(capture(requestSlot)) } returns mockCall
+
+        val targetFile = File.createTempFile("test", ".xml")
+
+        every { mockCall.execute() } returns mockResponse
+        every { mockResponse.isSuccessful } returns true
+        every { mockResponse.body } returns mockBody
+        every { mockBody.byteStream() } returns inputStream
+        every { mockResponse.close() } returns Unit
+
+        apiService.apiCallToFile("https://example.com", targetFile, acceptHeader = "application/xml")
+
+        Assertions.assertEquals("application/xml", requestSlot.captured.header("Accept"))
+
+        targetFile.deleteOnExit()
+    }
+
+    @Test
+    fun `apiCallToFile shouldn't contain acceptHeader when not provided`() {
+        //make sure to capture request sent
+        val requestSlot = slot<Request>()
+        every { mockClient.newCall(capture(requestSlot)) } returns mockCall
+
+        val targetFile = File.createTempFile("test", ".xml")
+
+        every { mockCall.execute() } returns mockResponse
+        every { mockResponse.isSuccessful } returns true
+        every { mockResponse.body } returns mockBody
+        every { mockBody.byteStream() } returns inputStream
+        every { mockResponse.close() } returns Unit
+
+        apiService.apiCallToFile("https://example.com", targetFile)
+
+        Assertions.assertEquals(null, requestSlot.captured.header("Accept"))
+
+        targetFile.deleteOnExit()
+    }
+}

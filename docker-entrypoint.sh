b691c42a2c7fd367806cc0b30ab631551c134f7f
NicolayLT <nicolayl@uia.no>
2026-02-27 15:08:44 +0100

    Changed all netex data file paths on kubernetes to tmp instead of app, also removed hardcoded netex data url in docker-entrypoint.sh

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -2,9 +2,7 @@
 
 echo "entrypoint"
 
-: ${NETEX_DATA_URL="https://storage.googleapis.com/marduk-test/outbound/netex/rb_avi-aggregated-netex.zip"}
-
 echo "Downloading $NETEX_DATA_URL"
-wget -O /app/netex_data.zip $NETEX_DATA_URL
+wget -O /tmp/netex_data.zip $NETEX_DATA_URL
 
 exec "$@"

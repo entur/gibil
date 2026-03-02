bbddd548b99a7799838f5e5f0c6ea2280e854c6a
NicolayLT <nicolayl@uia.no>
2026-03-02 10:23:18 +0100

    Moved configmap in values.yaml out of "container:" as well as moved the netex url in the different values files. Also added a default url as testing to see if it actually overrides to the correct one.

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -2,6 +2,8 @@
 
 echo "entrypoint"
 
+: ${NETEX_DATA_URL="https://storage.googleapis.com/marduk-test/outbound/netex/rb_avi-aggregated-netex.zip"}
+
 echo "Downloading $NETEX_DATA_URL"
 wget -O /tmp/netex_data.zip $NETEX_DATA_URL
 
